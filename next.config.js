3cafd864a3b2b320f022af59543e95b289faed8a
rsamec <roman.samec2@gmail.com>
2023-10-26 23:31:08 +0200

    add export flag

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const withExportImages = require('next-export-optimize-images')
 
 /** @type {import('next').NextConfig} */
-// const nextConfig = withExportImages({
-//   output:'export'
-// })
-const nextConfig = {  
-}
+const nextConfig = withExportImages({
+  output:'export'
+})
+// const nextConfig = {  
+// }
 
 module.exports = nextConfig
