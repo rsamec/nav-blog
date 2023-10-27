099f9f058016bd96b5548bae641a89a8cccb025c
rsamec <roman.samec2@gmail.com>
2023-10-27 09:12:42 +0200

    link disable prefetch

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -5,17 +5,17 @@ const Navigation = () => {
     <nav className="layout flex items-center justify-between py-4">
       <ul className="flex items-center justify-between space-x-3 text-xs md:space-x-4 md:text-base">
         <li>
-          <Link href="/" className="hover:underline">
+          <Link href="/" className="hover:underline" prefetch={false}>
             Home
           </Link>
         </li>
         <li>
-          <Link href="/#ships" className="hover:underline">
+          <Link href="/#ships" className="hover:underline" prefetch={false}>
             Lodě
           </Link>
         </li>
         <li>
-          <Link href="/#projects" className="hover:underline">
+          <Link href="/#projects" className="hover:underline" prefetch={false}>
             Rada a tipy
           </Link>
         </li>
